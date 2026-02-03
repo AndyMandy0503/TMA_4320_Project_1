25072e2a38011cfc1dc82f975afd89efb98429d0
Andreas Michaelsen <andreasmich02.work@gmail.com>
2026-02-03 23:52:15 +0100

    Update run_pinn.py

--- scripts/run_pinn.py
+++ scripts/run_pinn.py
@@ -22,6 +22,32 @@ def main():
     # Oppgave 5.4: Start
     #######################################################################
 
+    sensor_data = generate_training_data(cfg=cfg)
+    pinn_params, losses = train_pinn(sensor_data[4], cfg)
+    x = sensor_data[0]
+    y = sensor_data[1]
+    t = sensor_data[2]
+    T_pinn = predict_grid(nn_params=pinn_params['nn'], x=x, y=y, t=t, cfg=cfg)
+
+    plot_snapshots(
+        x, y, t, T_pinn, title='PINN', save_path='output/pinn/pinn_snapshots.png'
+    )
+    create_animation(
+        x, y, t, T_pinn, title='PINN', save_path='output/pinn/pinn_animation.gif'
+    )
+    plt.figure()
+    plt.plot(losses['total'], label='Total')
+    plt.plot(losses['data'], label='Data')
+    plt.plot(losses['ic'], label='IC')
+    plt.plot(losses['physics'], label='Physics')
+    plt.plot(losses['bc'], label='BC')
+
+    plt.xlabel('Epoke')
+    plt.ylabel('Tap')
+
+    plt.legend()
+    plt.savefig('output/pinn/pinn_losses.png')
+
     #######################################################################
     # Oppgave 5.4: Slutt
     #######################################################################
